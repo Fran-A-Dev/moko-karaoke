d35d137b5ba05449dbd12ef9d091979d464eefb9
Francis Agulto <francisagulto@MacBook-Pro-2.attlocal.net>
2022-03-27 10:52:32 -0500

    fixed app.js file

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
                 {!user && <Redirect to="/login" />}
                 {user && <Create />}
               </Route>
-              <Route path="/song/:id">
+              <Route path="/projects/:id">
                 {!user && <Redirect to="/login" />}
                 {user && <Project />}
               </Route>
